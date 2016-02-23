ea24230d8a5e0f7128418eb60b098223d5afc6e8
Thanasis Georgiou <contact@thgeorgiou.com>
2016-02-23 18:53:28 +0200

    Add demo app

---
+++ Demo C# App/Status_LED_LCD/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Status_LED_LCD {
+    class Program {
+        const int ClockSpeed = 15; // In Hz
+        const int Delay = 1000 / ClockSpeed;
+
+        static void Main(string[] args) {
+            Console.WriteLine("Clock (Hz): " +  ClockSpeed);
+
+            while (true) {
+                // Read one line
+                var text = Console.ReadLine();       // Read line
+                if (text.Length > 16) { text = text.Substring(0, 16); }
+                
+                var bits = new BitArray(
+                    Encoding.UTF8.GetBytes(
+                        text.ToCharArray()
+                        )
+                    ); 
+
+                StatusLED.NumLock = false;
+                StatusLED.CapsLock = false;
+                StatusLED.ScrollLock = true;
+
+                // New way
+                // Numlock: DATA 1
+                // CapsLock: DATA 2
+                // ScrollLock: CLK
+                for (var i = 0; i < bits.Length; i += 2) {
+                    StatusLED.NumLock = bits[i];
+                    StatusLED.CapsLock = bits[i + 1];
+
+                    Thread.Sleep(Delay / 3);
+                    StatusLED.ScrollLock = true;
+                    Thread.Sleep(Delay / 3);
+                    StatusLED.ScrollLock = false;
+                    Thread.Sleep(Delay / 3);
+                }
+
+                Thread.Sleep(1000);
+                StatusLED.NumLock = true;
+                StatusLED.CapsLock = false;
+            }
+        }
+    }
+}
---
+++ Demo C# App/Status_LED_LCD/StatusLED.cs
@@ -0,0 +1,102 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Status_LED_LCD {
+    /// <summary>
+    /// A simple API for controlling the keyboard status LEDs.
+    /// </summary>
+    class StatusLED {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true, CallingConvention = CallingConvention.Winapi)]
+        private static extern short GetKeyState(int keyCode);
+
+        [DllImport("user32.dll")]
+        private static extern int GetKeyboardState(byte[] lpKeyState);
+
+        [DllImport("user32.dll", EntryPoint = "keybd_event")]
+        private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+        private const byte VK_NUMLOCK = 0x90;
+        private const byte VK_CAPSLOCK = 0x14;
+        private const byte VK_SCROLL = 0x91;
+        private const uint KEYEVENTF_EXTENDEDKEY = 1;
+        private const int KEYEVENTF_KEYUP = 0x2;
+        private const int KEYEVENTF_KEYDOWN = 0x0;
+
+        /// <summary>
+        /// Get the current state of num lock
+        /// </summary>
+        /// <returns>True for Num Lock on, false for off.</returns>
+        private static bool GetNumLock() {
+            return (((ushort)GetKeyState(VK_NUMLOCK)) & 0xffff) != 0;
+        }
+
+        /// <summary>
+        /// Get the current state of caps lock
+        /// </summary>
+        /// <returns>True for Caps Lock on, false for off.</returns>
+        private static bool GetCapsLock() {
+            return (((ushort)GetKeyState(VK_CAPSLOCK)) & 0xffff) != 0;
+        }
+
+        /// <summary>
+        /// Get the current state of scroll lock
+        /// </summary>
+        /// <returns>True for Scroll Lock on, false for off.</returns>
+        private static bool GetScrollLock() {
+            return (((ushort)GetKeyState(VK_SCROLL)) & 0xffff) != 0;
+        }
+
+        /// <summary>
+        /// Keyboard status LED for Num Lock
+        /// </summary>
+        public static bool NumLock
+        {
+            get
+            {
+                return GetNumLock();
+            }
+            set
+            {
+                if (GetNumLock() != value) {
+                    keybd_event(VK_NUMLOCK, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYDOWN, 0);
+                    keybd_event(VK_NUMLOCK, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Keyboard status LED for Caps Lock
+        /// </summary>
+        public static bool CapsLock
+        {
+            get
+            {
+                return GetCapsLock();
+            }
+            set
+            {
+                if (GetCapsLock() != value) {
+                    keybd_event(VK_CAPSLOCK, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYDOWN, 0);
+                    keybd_event(VK_CAPSLOCK, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Keyboard status LED for Scroll Lock
+        /// </summary>
+        public static bool ScrollLock
+        {
+            get
+            {
+                return GetScrollLock();
+            }
+            set
+            {
+                if (GetScrollLock() != value) {
+                    keybd_event(VK_SCROLL, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYDOWN, 0);
+                    keybd_event(VK_SCROLL, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+                }
+            }
+        }
+    }
+}
